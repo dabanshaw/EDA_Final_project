74c6c1bae2d884b363cd99567da84d9187bf6aa5
Sean053047 <sadlamb910803@gmail.com>
2024-06-05 02:57:46 +0800

    [mod] Fix errors.

--- main.cpp
+++ main.cpp
@@ -10,12 +10,10 @@ using json = nlohmann::json;
 int main(int argc, char**argv) {
     vector<Block> res_blocks;
     vector<Connection_Matrix> res_connections;
-<<<<<<< HEAD
-    tie(res_blocks, res_connections) = read(argv[2], argv[3], argv[4]);
-
-=======
+    vector<Region> regions;
+    vector<Point> chip_layout;
     tie(res_blocks, res_connections) = read_json(argv[3], argv[4]);
-    update_blk_coor(argv[2], res_blocks);
->>>>>>> 6572494567b7158a1e723993527223c1c7e7e211
+    tie(chip_layout, regions) = read_def(argv[2], res_blocks);
+    
     return 0;
 }
