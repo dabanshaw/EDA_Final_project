e3420e30152e962dc0cd104cc73c05f1df495c4c
Sean053047 <sadlamb910803@gmail.com>
2024-06-07 02:55:55 +0800

    [mod] modify tmp

---
+++ header/graph.h
@@ -0,0 +1,13 @@
+# ifndef GRAPH_H
+# define GRAPH_H
+# include <vector>
+# include "./module.h"
+# include <string>
+# include <tuple>
+# include <vector>
+
+using namespace std;
+
+void determine_vertex(int tracks_per_um, int length, int width, vector<Connection_Matrix> &connections, vector<Block> &blocks);
+
+# endif
